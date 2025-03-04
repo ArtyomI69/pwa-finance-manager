da122659ee24a2fb4a40fff7b18bea25c5951c2a
Artyom Ivanov <Artyom655@yandex.ru>
2025-03-04 17:17:38 +0300

    navlink for applestyledock

--- src/widgets/AppleStyleDock/AppleStyleDock.tsx
+++ src/widgets/AppleStyleDock/AppleStyleDock.tsx
@@ -1,20 +1,32 @@
 import { NavItem } from '@/feautres/Navigation';
 import { Dock, DockIcon, DockItem, DockLabel } from '../../shared/components/ui/dock';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export function AppleStyleDock({ navItems }: { navItems: NavItem[] }) {
   return (
     <div className="fixed z-50 bottom-2 left-1/2 max-w-full -translate-x-1/2">
       <Dock className="items-end pb-3">
         {navItems.map((item) => (
-          <Link to={item.href} key={item.title}>
-            <DockItem className="aspect-square rounded-full bg-gray-200 dark:bg-neutral-800">
-              <DockLabel>{item.title}</DockLabel>
-              <DockIcon className="text-neutral-600 dark:text-neutral-300 h-full w-full">
-                {item.icon}
-              </DockIcon>
-            </DockItem>
-          </Link>
+          <NavLink to={item.href} key={item.title}>
+            {({ isActive }) => (
+              <DockItem
+                className={`aspect-square rounded-full ${
+                  isActive ? 'bg-gray-400 dark:bg-neutral-600' : 'bg-gray-200 dark:bg-neutral-800'
+                }`}
+              >
+                <DockLabel>{item.title}</DockLabel>
+                <DockIcon
+                  className={`${
+                    isActive
+                      ? 'text-neutral-900 dark:text-neutral-500'
+                      : 'text-neutral-700 dark:text-neutral-300'
+                  } h-full w-full`}
+                >
+                  {item.icon}
+                </DockIcon>
+              </DockItem>
+            )}
+          </NavLink>
         ))}
       </Dock>
     </div>
