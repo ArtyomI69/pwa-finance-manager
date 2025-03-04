2257a515f697670cad5010b52828cd07ded23339
Artyom Ivanov <Artyom655@yandex.ru>
2025-03-04 17:09:35 +0300

    add magicui bento grid

--- src/pages/ReceiptsPage/ui/ReceiptsPage.tsx
+++ src/pages/ReceiptsPage/ui/ReceiptsPage.tsx
@@ -1,43 +1,63 @@
-const ReceiptsPage = () => {
+import { BellIcon, CalendarIcon, FileTextIcon, GlobeIcon, InputIcon } from '@radix-ui/react-icons';
+
+import { BentoCard, BentoGrid } from '@/shared/components/magicui/bento-grid';
+
+const features = [
+  {
+    Icon: FileTextIcon,
+    name: 'Save your files',
+    description: 'We automatically save your files as you type.',
+    href: '/',
+    cta: 'Learn more',
+    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    className: 'md:row-start-1 md:row-end-4 md:col-start-2 md:col-end-3',
+  },
+  {
+    Icon: InputIcon,
+    name: 'Full text search',
+    description: 'Search through all your files in one place.',
+    href: '/',
+    cta: 'Learn more',
+    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    className: 'md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-3',
+  },
+  {
+    Icon: GlobeIcon,
+    name: 'Multilingual',
+    description: 'Supports 100+ languages and counting.',
+    href: '/',
+    cta: 'Learn more',
+    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    className: 'md:col-start-1 md:col-end-2 md:row-start-3 md:row-end-4',
+  },
+  {
+    Icon: CalendarIcon,
+    name: 'Calendar',
+    description: 'Use the calendar to filter your files by date.',
+    href: '/',
+    cta: 'Learn more',
+    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    className: 'md:col-start-3 md:col-end-3 md:row-start-1 md:row-end-2',
+  },
+  {
+    Icon: BellIcon,
+    name: 'Notifications',
+    description: 'Get notified when someone shares a file or mentions you in a comment.',
+    href: '/',
+    cta: 'Learn more',
+    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    className: 'md:col-start-3 md:col-end-3 md:row-start-2 md:row-end-4',
+  },
+];
+
+function ReceiptsPage() {
   return (
-    <div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-      <div>ReceiptsPage</div>
-    </div>
+    <BentoGrid className="md:grid-rows-3">
+      {features.map((feature) => (
+        <BentoCard key={feature.name} {...feature} />
+      ))}
+    </BentoGrid>
   );
-};
+}
 
 export default ReceiptsPage;
