9e70927ed2ed48ee41229a4f1e6fdbdd35beca08
Artyom Ivanov <Artyom655@yandex.ru>
2025-03-08 01:23:11 +0300

    geocode demo

--- src/feautres/ReceiptsMap/ui/ReceiptsMap.tsx
+++ src/feautres/ReceiptsMap/ui/ReceiptsMap.tsx
@@ -1,6 +1,30 @@
 import { Map, Placemark } from '@pbe/react-yandex-maps';
+import { useState } from 'react';
 
 export const ReceiptsMap = () => {
+  const [coordinates, setCoordinates] = useState([55.751574, 37.573856]); // Москва по умолчанию
+
+  // useEffect(() => {
+  //   const getCoordinates = async () => {
+  //     const response = await fetch(
+  //       'https://geocode-maps.yandex.ru/1.x/?apikey=da69f6d8-f14f-4c6a-a65a-8093a117cfef&geocode=Москва+Красная+Площадь&format=json'
+  //     );
+  //     const result = await response.json();
+  //     console.log(
+  //       result.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+  //         .split(' ')
+  //         .reverse()
+  //     );
+  //     setCoordinates(
+  //       result.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+  //         .split(' ')
+  //         .reverse()
+  //     );
+  //   };
+
+  //   getCoordinates();
+  // }, []);
+
   return (
     <Map
       className="flex-1"
@@ -10,7 +34,7 @@ export const ReceiptsMap = () => {
       }}
     >
       <Placemark
-        geometry={[55.684758, 37.738521]}
+        geometry={coordinates}
         options={{
           iconColor: 'red',
           preset: 'islands#blueMoneyCircleIcon',
@@ -20,8 +44,17 @@ export const ReceiptsMap = () => {
         modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
         defaultProperties={{
           hintContent: 'Заголовок метки',
+          iconCaption: 'Магазин',
           balloonContentHeader: 'Заголовок метки',
-          balloonContentBody: 'Описание метки',
+          balloonContentBody: `
+              <div>
+                <p>Описание метки</p>
+                <button onClick={console.log('test')} id="myButton" style="padding: 5px 10px; background: blue; color: white; border: none; cursor: pointer;">
+                  Нажми меня
+                </button>
+              </div>
+            `,
+
           balloonContentFooter: 'Дополнительная информация',
         }}
       />
--- src/pages/ReceiptsMapPage/ui/ReceiptsMapPage.tsx
+++ src/pages/ReceiptsMapPage/ui/ReceiptsMapPage.tsx
@@ -4,7 +4,7 @@ import { YMaps } from '@pbe/react-yandex-maps';
 const ReceiptsMapPage = () => {
   return (
     <YMaps
-    //  query={{ apikey: 'da69f6d8-f14f-4c6a-a65a-8093a117cfef' }}
+    // query={{ apikey: 'da69f6d8-f14f-4c6a-a65a-8093a117cfef' }}
     >
       <ReceiptsMap />
     </YMaps>
