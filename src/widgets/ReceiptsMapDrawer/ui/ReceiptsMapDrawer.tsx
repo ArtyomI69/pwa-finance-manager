a22ab9afbf1cd1b4cc6784f71984918e437208b9
Artyom Ivanov <Artyom655@yandex.ru>
2025-03-09 01:13:39 +0300

    add receipts map drawer adaptive for phones

--- src/widgets/ReceiptsMapDrawer/ui/ReceiptsMapDrawer.tsx
+++ src/widgets/ReceiptsMapDrawer/ui/ReceiptsMapDrawer.tsx
@@ -1,10 +1,26 @@
 import { MenuIcon } from '@/shared/assets/icons/menu-icon';
 import { Button } from '@/shared/components/bucketsui/button';
 import { Drawer } from '@/shared/components/bucketsui/drawer';
+import { useIsMobile } from '@/shared/hooks/use-mobile';
+import { useState } from 'react';
 
 export const ReceiptsMapDrawer = () => {
+  const isMobile = useIsMobile();
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean | undefined>();
+
+  const showDrawer = () => {
+    setIsDrawerOpen(undefined);
+  };
+
+  const hideDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
     <Drawer
+      modal={isMobile ? undefined : false}
+      open={isMobile ? isDrawerOpen : true}
+      onOpenChange={isMobile ? showDrawer : undefined}
       position="left"
       trigger={
         <Button appearance="text" className="w-12 h-12 md:w-10 md:h-10">
@@ -13,7 +29,7 @@ export const ReceiptsMapDrawer = () => {
       }
     >
       <div className="flex flex-col gap-2 p-8 w-full">
-        <h3 className="font-blink-title text-4xl italic">Panel title</h3>
+        <h3 className="font-blink-title text-3xl italic">Настройка карты</h3>
       </div>
     </Drawer>
   );
