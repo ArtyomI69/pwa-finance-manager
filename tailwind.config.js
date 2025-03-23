333c69e18ba18d2088cf29b77fdd986e14a4751d
Artyom Ivanov <Artyom655@yandex.ru>
2025-03-23 18:17:35 +0300

    install tremor dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 import * as BucketsUI from './configs/tailwind/bucketsui';
 import * as ShadcnUI from './configs/tailwind/shadcnui';
+import * as Tremor from './configs/tailwind/tremor';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,tsx,jsx}'],
   theme: {
     extend: {
-      keyframes: { ...BucketsUI.keyframes },
-      animation: { ...BucketsUI.animation },
+      keyframes: { ...BucketsUI.keyframes, ...Tremor.keyframes },
+      animation: { ...BucketsUI.animation, ...Tremor.animation },
       fontFamily: { ...BucketsUI.fontFamily },
       borderRadius: { ...ShadcnUI.borderRadius },
       colors: { ...ShadcnUI.colors, ...BucketsUI.colors },
     },
   },
-  plugins: [],
+  plugins: [...Tremor.plugins],
 };
