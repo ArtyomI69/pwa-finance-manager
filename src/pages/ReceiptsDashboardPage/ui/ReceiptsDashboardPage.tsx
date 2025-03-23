e46977cbe03ec3c051054b7c87f8aef8d9525b0b
Artyom Ivanov <Artyom655@yandex.ru>
2025-03-23 17:47:31 +0300

    layout with tabs for receiptsDashboardPage

--- src/pages/ReceiptsDashboardPage/ui/ReceiptsDashboardPage.tsx
+++ src/pages/ReceiptsDashboardPage/ui/ReceiptsDashboardPage.tsx
@@ -1,5 +1,24 @@
+import { DatePickerWithRange } from '@/shared/components/shadcnui/data-range-picker';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shared/components/shadcnui/ui/tabs';
+
 const ReceiptsDashboardPage = () => {
-  return <div>ReceiptsDashboardPage</div>;
+  return (
+    <div className="max-w-screen-lg mx-auto w-full py-8 flex flex-col gap-4">
+      <DatePickerWithRange className="w-fit mx-auto" />
+      <Tabs defaultValue="personal" className="flex flex-col overflow-hidden">
+        <TabsList className="grid flex-1 grid-cols-2">
+          <TabsTrigger value="personal">Персональное</TabsTrigger>
+          <TabsTrigger value="group">Груповое</TabsTrigger>
+        </TabsList>
+        <TabsContent value="personal" className="flex-1 overflow-y-scroll">
+          <div className="bg-red-300">kek</div>
+        </TabsContent>
+        <TabsContent value="group" className="flex-1 overflow-y-scroll">
+          <div className="bg-blue-300">lol</div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ReceiptsDashboardPage;
