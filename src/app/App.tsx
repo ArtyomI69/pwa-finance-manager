57149b4f413ab5aea8ed87a64f4d3fab19f2de83
Artyom Ivanov <Artyom655@yandex.ru>
2025-04-19 00:13:27 +0300

    add register page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,22 @@
-import { Navigation } from '@/widgets/Navigation';
 import { Suspense } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Navigation } from '@/widgets/Navigation';
 import AppRouter from './providers/router/ui/AppRouter';
 import { ColorActivator } from '@/shared/components/ui/ColorActivator';
+import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
 
 export const App = () => {
+  const location = useLocation();
+  const isRegisterPage = location.pathname.includes(RoutePath.register);
+  const isLoginPage = location.pathname.includes(RoutePath.login);
+  const isAuthPage = isRegisterPage || isLoginPage;
+
   return (
     <Suspense fallback={''}>
       <div className="min-h-[calc(100vh-63.2px)] md:min-h-screen flex flex-col">
         <AppRouter />
       </div>
-      <Navigation />
+      {!isAuthPage && <Navigation />}
       <ColorActivator />
     </Suspense>
   );
---
+++ src/pages/RegisterPage/ui/RegisterPage.tsx
@@ -0,0 +1,174 @@
+'use client';
+
+import { Link } from 'react-router-dom';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/shared/components/shadcnui/ui/form';
+import { Button } from '@/shared/components/shadcnui/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/shared/components/shadcnui/ui/card';
+import { Input } from '@/shared/components/shadcnui/ui/input';
+import { PasswordInput } from '@/shared/components/shadcn-form/ui/password-input';
+import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
+
+// Define validation schema using Zod
+const formSchema = z
+  .object({
+    name: z.string().min(2, { message: 'Name must be at least 2 characters long' }),
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters long' })
+      .regex(/[a-zA-Z0-9]/, { message: 'Password must be alphanumeric' }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Passwords do not match',
+  });
+
+export function RegisterPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      // Assuming an async registration function
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error('Form submission error', error);
+      toast.error('Failed to submit the form. Please try again.');
+    }
+  }
+
+  return (
+    <div className="flex flex-1 min-h-[60vh] h-full w-full items-center justify-center px-4">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Регистрация</CardTitle>
+          <CardDescription>Создайте новую учетную запись, заполнив форму ниже.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="grid gap-4">
+                {/* Name Field */}
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="name">Имя</FormLabel>
+                      <FormControl>
+                        <Input id="name" placeholder="Имя" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Email Field */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="email">Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          id="email"
+                          placeholder="johndoe@mail.com"
+                          type="email"
+                          autoComplete="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Password Field */}
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="password">Пароль</FormLabel>
+                      <FormControl>
+                        <PasswordInput
+                          id="password"
+                          placeholder="******"
+                          autoComplete="new-password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Confirm Password Field */}
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="confirmPassword">Подтвердите пароль</FormLabel>
+                      <FormControl>
+                        <PasswordInput
+                          id="confirmPassword"
+                          placeholder="******"
+                          autoComplete="new-password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit" className="w-full">
+                  Создать учётную запись
+                </Button>
+              </div>
+            </form>
+          </Form>
+          <div className="mt-4 text-center text-sm">
+            Уже есть учётная запись?{' '}
+            <Link to={RoutePath.login} className="underline">
+              Войти
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
