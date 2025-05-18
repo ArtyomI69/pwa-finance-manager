f1ab12ac0f1661c6174fe201c59d42a1f6981549
Artyom Ivanov <Artyom655@yandex.ru>
2025-05-18 23:53:32 +0300

    add pdftotext

--- src/pages/BanksDashboardPage/ui/BanksDashboardPage.tsx
+++ src/pages/BanksDashboardPage/ui/BanksDashboardPage.tsx
@@ -1,3 +1,25 @@
+import pdfToText from 'react-pdftotext';
+
 export default function BanksDashboardPage() {
-  return <div className="max-w-screen-lg mx-auto w-full py-8 flex flex-col gap-4">dsadasdsa</div>;
+  const onFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (!files) return;
+
+    const file = files[0];
+    pdfToText(file)
+      .then((text) => console.log(text))
+      .catch((error) => console.error('Failed to extract text from pdf'));
+  };
+
+  return (
+    <div className="max-w-screen-lg mx-auto w-full py-8 flex flex-col gap-4">
+      <input
+        tabIndex={-1}
+        type="file"
+        onChange={onFileChange}
+        accept=".pdf"
+        className="absolute top-0 left-0 opacity-0"
+      />
+    </div>
+  );
 }
