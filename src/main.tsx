9ae09a7d0dee44f57a5879333cb1e9efb9453db7
Artyom Ivanov <Artyom655@yandex.ru>
2025-05-27 21:04:33 +0300

    add real pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/App';
 import './app/styles/index.css';
 
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
+
 const container = document.querySelector('#root') as HTMLElement;
 
 const root = ReactDOM.createRoot(container);
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react';
 import { babel } from '@rollup/plugin-babel';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const manifest: any = {
+  theme_color: '#96ff94',
+  background_color: '#ffffff',
+  icons: [
+    { purpose: 'maskable', sizes: '512x512', src: 'icon512_maskable.png', type: 'image/png' },
+    { purpose: 'any', sizes: '512x512', src: 'icon512_rounded.png', type: 'image/png' },
+  ],
+  orientation: 'any',
+  display: 'standalone',
+  dir: 'auto',
+  lang: 'ru',
+  name: 'Финансы семьи: анализ и учёт',
+  short_name: 'Финансы семьи: анализ и учёт',
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,6 +27,10 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['robots.txt'],
+      workbox: {
+        globPatterns: ['**/*.{html,css,js,ico,png,svg,jpg}'],
+      },
+      manifest,
     }),
   ],
   resolve: {
