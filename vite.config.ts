990148bbad13fee43d3a178c2877b3d14527c702
Artyom Ivanov <Artyom655@yandex.ru>
2025-06-06 01:27:47 +0300

    add offline mode

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,43 @@ export default defineConfig({
       includeAssets: ['robots.txt'],
       workbox: {
         globPatterns: ['**/*.{html,css,js,ico,png,svg,jpg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /\/auth\/.*/i, // Регулярное выражение для API endpoints
+            handler: 'NetworkFirst', // Стратегия кэширования
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24, // 1 день
+              },
+              networkTimeoutSeconds: 10, // Таймаут для NetworkFirst стратегии
+            },
+          },
+          {
+            urlPattern: /\/rest\/.*/i, // Регулярное выражение для API endpoints
+            handler: 'NetworkFirst', // Стратегия кэширования
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24, // 1 день
+              },
+              networkTimeoutSeconds: 10, // Таймаут для NetworkFirst стратегии
+            },
+          },
+          {
+            urlPattern: /\/storage\/.*/i, // Пример для статических файлов
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'static-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 дней
+              },
+            },
+          },
+        ],
       },
       manifest,
     }),
